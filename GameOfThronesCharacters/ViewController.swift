86eb39a821590052b39c43a943b5b8af6678ecce
Bengi Anıl <bengianil34@gmail.com>
2021-10-14 20:40:47 +0300

    Initial Commit

---
+++ GameOfThronesCharacters/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameOfThronesCharacters
+//
+//  Created by Bengi on 14.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
